44cf1ca71d1f71c0aa48089d842593225c97bbf2
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2025-04-24 11:15:07 +0200

    Implement REST API (#2)
    Co-authored-by: Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>

---
+++ customer-api-provider/src/main/java/de/schulung/quarkus/CustomersResource.java
@@ -0,0 +1,81 @@
+package de.schulung.quarkus;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Path("/customers")
+public class CustomersResource {
+
+  @Context
+  UriInfo uriInfo;
+
+  // TODO replace it
+  private final Map<UUID, Customer> customers = new HashMap<>();
+
+  {
+    Customer customer = new Customer();
+    customer.setUuid(UUID.randomUUID());
+    customer.setName("Tom Mayer");
+    customer.setBirthdate(LocalDate.now().minusYears(30));
+    customer.setState("active");
+    customers.put(customer.getUuid(), customer);
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Collection<Customer> getCustomers() {
+    return this
+      .customers
+      .values();
+  }
+
+  @GET
+  @Path("/{uuid}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getCustomerById(
+    @PathParam("uuid") UUID uuid
+  ) {
+    var customer = customers.get(uuid);
+    if (null == customer) {
+      return Response.status(404).build();
+    } else {
+      return Response.ok(customer).build();
+    }
+  }
+
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  // @Consumes(MediaType.APPLICATION_JSON)
+  public Response createCustomer(
+    Customer customer
+  ) {
+    if (customer.getUuid() != null) {
+      return Response.status(400).build();
+    }
+    customer.setUuid(UUID.randomUUID());
+    this.customers.put(customer.getUuid(), customer);
+    return Response
+      .created(
+        uriInfo
+          .getAbsolutePathBuilder()
+          .path(customer.getUuid().toString())
+          .build()
+      )
+      .entity(customer)
+      .build();
+  }
+
+}
