86869535440db5090904c347ede4494b553879ba
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2025-04-25 13:21:32 +0200

    Make initializer configurable. (#10)
    * Make initializer configurable.
    * Add tests.
    
    ---------
    
    Co-authored-by: Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>

--- customer-api-provider/src/test/java/de/schulung/quarkus/CustomerInitializerTests.java
+++ customer-api-provider/src/test/java/de/schulung/quarkus/CustomerInitializerTests.java
@@ -1,21 +1,66 @@
 package de.schulung.quarkus;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
-public class CustomerInitializerTests {
+class CustomerInitializerTests {
+
+  public static class InitializationEnabledProfile
+    implements QuarkusTestProfile {
+
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return Map.of(
+        "customers.initialization.enabled", "true"
+      );
+    }
+  }
+
+  public static class InitializationDisabledProfile
+    implements QuarkusTestProfile {
+
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return Map.of(
+        "customers.initialization.enabled", "false"
+      );
+    }
+  }
 
   @Inject
   CustomersService customersService;
 
-  @Test
-  void whenInit_thenCustomersNotEmpty() {
-    assertThat(customersService.count())
-      .isGreaterThan(0L);
+  @Nested
+  @TestProfile(InitializationEnabledProfile.class)
+  class InitializationEnabledProfileTests {
+
+    @Test
+    void whenInit_thenCustomersNotEmpty() {
+      assertThat(customersService.count())
+        .isGreaterThan(0L);
+    }
+
+  }
+
+  @Nested
+  @TestProfile(InitializationDisabledProfile.class)
+  class InitializationDisabledProfileTests {
+
+    @Test
+    void whenInit_thenCustomersEmpty() {
+      assertThat(customersService.count())
+        .isEqualTo(0L);
+    }
+
   }
 
 }
